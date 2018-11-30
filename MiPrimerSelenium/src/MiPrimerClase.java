4c1ca50198171899e9d43252ce76479bc5c5ee2c
Catalina Mesa <kta_mesa@yahoo.es>
2018-11-29 19:48:09 -0500

    Documentcion de las clases

--- MiPrimerSelenium/src/MiPrimerClase.java
+++ MiPrimerSelenium/src/MiPrimerClase.java
@@ -1,30 +1,33 @@
-import org.openqa.selenium.chrome.ChromeDriver; //seleccionar el controlador 
-import org.openqa.selenium.WebDriver; //Manipular... voy a utilizar la clase web driver
-import org.openqa.selenium.By; //para localizar por algo Id, nombre)
+/*Mi primer Selenium
+ * Utilizando Google Chrome
+ * Autora: Catalina Mesa
+ */
+//Importar las librerias para Chrome
+import org.openqa.selenium.chrome.ChromeDriver; 
+//Importar la libreria para utilizar el WebDriver
+import org.openqa.selenium.WebDriver; 
+//Importar la libreria para que el WebDriver localice algún objeto por id, nombre, xpath,etc.
+import org.openqa.selenium.By; 
 
 public class MiPrimerClase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//Paso 2 Ubicar el controlador
-		//El controlador se encuentra en la direccione especifica
+
+		//Dirigir el sistema al controlador de google chrome y le damos la ubicacion en el sistema en el segundo parametro
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver_win32\\chromedriver.exe"); //
 		
 		//Crear el robot que nombro Daniel
 		WebDriver Daniel = new ChromeDriver(); //el robot que abre el navergador lo delcaro, crea una variable del sistema
-		//Hasta este punto se logra abrir el navegado 
-		
+
 		//Especifico la direccion que quiero trabajar
 		Daniel.get("http://www.google.com");
 		
-		//que google me bsuque la cajita de texto
+		//Le digo al WebDriver que google me busque la cajita de texto
 		Daniel.findElement(By.name("q")).sendKeys("Suso");
 		
-		//Encuentra con el Id del elemento que seria la caja de texto
-		//Daniel.findElement(By.id("lst-ib")).clear();
-	//hacer click al boton buscar-- hacerlo 
-				
+		//Daniel sale del explorador
+		Daniel.close();
+		
 
 	}
 
--- MiPrimerSelenium/src/TareaFirefox.java
+++ MiPrimerSelenium/src/TareaFirefox.java
@@ -1,27 +1,37 @@
-import org.openqa.selenium.firefox.FirefoxDriver; //seleccionar el controlador 
-import org.openqa.selenium.WebDriver; //Manipular... voy a utilizar la clase web driver
-import org.openqa.selenium.By; //para localizar por algo Id, nombre)
+/*Mi primer Selenium
+ * Utilizando Firefox
+ * Autora: Catalina Mesa
+ */
+
+//Importar las librerias para firefox
+import org.openqa.selenium.firefox.FirefoxDriver;
+//Importar la libreria para utilizar el WebDriver
+import org.openqa.selenium.WebDriver; 
+//Importar la libreria para que el WebDriver localice algún objeto por id, nombre, xpath,etc.
+import org.openqa.selenium.By; 
+
 public class TareaFirefox {
 
 	
 	public static void main(String[] args) {
-	
-		//Paso 2 Ubicar la carpeta que contiene el controlador
+
 		//Dirigir el sistema al controlador de firefox que se llama gecko (intermediario entre firefox y selenium) y le damos la ubicacion en el sistema en el segundo parametro
 		System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\geckodriver-v0.23.0-win32\\geckodriver.exe"); //
 		
 		//Creo y declaro el webdriver  - mi robot que se llama Antonio
 		WebDriver antonio = new FirefoxDriver(); //declaro el robot que me va hacer las busquedas
-		//Hasta este punto se logra abrir el navegado 
 		
-		//Dar la orden al webdriver de ir a la pagina indicada
+		//Doy la orden al webdriver de ir a la pagina indicada
 		antonio.get("https://www.google.com.co");
 		
-		//le digo al webdriver que busque el texbox que se llama q y digite el ultimo sismo
+		//Le digo al webdriver que busque el texbox que se llama q y digíte el último sismo
 		antonio.findElement(By.name("q")).sendKeys("ultimo sismo");
 		
-		//Le digo al robot que me encuentre el boton de buscar y le de click
+		//Le digo al robot que me encuentre el botón de buscar y le de click
 		antonio.findElement(By.name("btnK")).click();
+		
+		//El webDriver se cierra
+		antonio.close();
 
 
 	}
